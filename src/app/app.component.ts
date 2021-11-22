398513c31b153a9ead614353342f49a6a8e6d8c8
ignacio <ignasi44@hotmail.com>
2021-11-22 17:55:26 +0100

    Terminado header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from './app.reducers';
+import * as AuthAction from './Auth/actions';
 import { LoadingService } from './Shared/Services/loading.service';
 
 @Component({
@@ -9,5 +13,55 @@ import { LoadingService } from './Shared/Services/loading.service';
 export class AppComponent {
   title = 'blog-uoc-project-front';
   loading$ = this.loader.loading$;
-  constructor(public loader: LoadingService) {}
+  showAuthSection: boolean;
+  showNoAuthSection: boolean;
+
+  constructor(
+    public loader: LoadingService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {
+    this.showAuthSection = false;
+    this.showNoAuthSection = true;
+  }
+
+  ngOnInit(): void {
+    this.store.select('auth').subscribe((auth) => {
+      this.showAuthSection = false;
+      this.showNoAuthSection = true;
+      if (auth.credentials.access_token) {
+        this.showAuthSection = true;
+        this.showNoAuthSection = false;
+      }
+    });
+  }
+
+  home(): void {
+    this.router.navigateByUrl('home');
+  }
+
+  login(): void {
+    this.router.navigateByUrl('login');
+  }
+
+  register(): void {
+    this.router.navigateByUrl('register');
+  }
+
+  adminPosts(): void {
+    this.router.navigateByUrl('posts');
+  }
+
+  adminCategories(): void {
+    this.router.navigateByUrl('categories');
+  }
+
+  profile(): void {
+    this.router.navigateByUrl('profile');
+  }
+
+  logout(): void {
+    this.store.dispatch(AuthAction.logout());
+    this.router.navigateByUrl('home');
+  }
 }
